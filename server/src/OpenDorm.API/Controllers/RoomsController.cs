f3a3b2acebc2204d36046bd5446789bf82865b6d
Dmitry <dreamysnail0921@gmail.com>
2026-09-25 18:13:42 +0500

    feat-api: add endpoint GET: api/rooms/{id}/history.

--- server/src/OpenDorm.API/Controllers/RoomsController.cs
+++ server/src/OpenDorm.API/Controllers/RoomsController.cs
@@ -1,5 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using OpenDorm.Application.Common;
+using OpenDorm.Application.Features.Dormitories.Queries.GetRoomOccupancyHistory;
 using OpenDorm.Application.Features.Dormitories.Queries.GetRoomOccupants;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -20,6 +22,27 @@ public class RoomsController(IMediator mediator) : ControllerBase
         CancellationToken cancellationToken)
     {
         var query = new GetRoomOccupantsQuery(id);
+        var response = await mediator.Send(query, cancellationToken);
+
+        return Ok(response);
+    }
+    
+    // GET: api/rooms/{id}/history
+    [HttpGet("{id:guid}/history")]
+    [SwaggerOperation("Получить историю заселений для комнаты.")]
+    [ProducesResponseType(typeof(PagedResult<RoomOccupancyHistoryListDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetHistory(
+        Guid id,
+        CancellationToken cancellationToken,
+        SortOrder sortOrder = SortOrder.Descending,
+        DateOnly? startCheckInDate = null,
+        DateOnly? endCheckInDate = null,
+        int page = 1,
+        int pageSize = 20)
+    {
+        var query = new GetRoomOccupancyHistoryQuery(id, sortOrder, startCheckInDate, endCheckInDate, page, pageSize);
         var response = await mediator.Send(query, cancellationToken);
 
         return Ok(response);
